cda3034380673fbaa23067f0b387ea6e62904a36
Daniel Garcia <dvaldez@ilsp.com.mx>
2019-03-28 12:21:07 -0600

    nueva correcion1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-/** @params someone */
+  /** 
+  * @param someone
+  * @return string data
+  */
 
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
